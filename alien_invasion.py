5ae70de65b7780a2d77fa0ac8008de3b922182cf
Rodrigo_Almeida <rodrigoap1307@gmail.com>
2020-01-08 18:22:13 -0300

    Botao PLAY adcionado

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from game_stats import GameStats
+from button import Button
 
 
 def run_game():
@@ -13,6 +14,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
+    # Cria o botão play
+    play_button = Button(ai_settings, screen, "Play")
 
     # Cria uma instância para armazenar dados estatisticos do jogo
     stats = GameStats(ai_settings)
@@ -36,7 +40,7 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -48,7 +48,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Atualiza as imagens na tela e alterna para a nova tela"""
     # Redesenha a tela a cada passagem pelo laço
     screen.fill(ai_settings.bg_color)
@@ -58,6 +58,12 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # Desenha o botão play se o jogo estiver INATIVO
+    if not stats.game_active:
+        play_button.draw_button()
+
+
 
     # Deixa a tela mais recente vísivel
     pygame.display.flip()
