fcc93285c2ab0a828e3337ac8831a72984a58905
RodrigoAP1307 <rodrigoap1307@gmail.com>
2020-01-09 11:05:52 -0300

    Bug do play funcionando depois de clicado RESOLVIDO

--- game_functions.py
+++ game_functions.py
@@ -53,7 +53,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Inicia um novo jogo quando o jogador clicar em Play"""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         # Reinicia os dados estatisticos do jogo
         stats.reset_stats()
         stats.game_active = True
